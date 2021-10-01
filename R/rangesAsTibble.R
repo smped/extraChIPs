68e748ebfc4638960792f0f21f0aa8f44d5158c7
Steve Pederson <stephen.pederson.au@gmail.com>
2021-10-02 01:01:28 +0930

    Changed rangesAsTibble to S3 as_tibble method

--- R/rangesAsTibble.R
+++ R/rangesAsTibble.R
@@ -1,20 +1,17 @@
-#' @title Convert GRanges to a tibble
+#' @title Convert GRanges or DFrame to a tibble
 #'
-#' @description Convert GRanges objects to tibble objects
+#' @description Convert GRanges or DataFrame objects to tibble objects
 #'
 #' @details
-#' Simple conversion of a GRanges object to a tibble. The ranges will be
-#' returned as a character column called `ranges`. Seqinfo information will be
+#' Quick and dirty conversion into a tibble. The ranges will be
+#' returned as a character column called `range`. Seqinfo information will be
 #' lost.
 #'
-#' @param x A Genomic Ranges object
-#' @param var Name of column to use for ranges
-#' @param drop.existing logical(1). Overwrite any existing columns with the
-#' same name as supplied in var
+#' @param x A Genomic Ranges or DataFrame object
+#' @param name Name of column to use for ranges
+#' @param rangeAsChar Convert the GRanges element to a character vector
+#' @param ... Not used
 #'
-#' @importFrom tibble as_tibble
-#' @importFrom GenomicRanges granges
-#' @importFrom S4Vectors mcols
 #'
 #' @return
 #' A \link[tibble]{tibble}
@@ -22,30 +19,27 @@
 #' @examples
 #' gr <- GRanges("chr1:1-10")
 #' gr$p_value <- runif(1)
-#' rangesAsTibble(gr)
-#' mcolsAsTibble(gr)
+#' as_tibble(mcols(gr))
+#' as_tibble(gr)
 #'
-#' @export
-rangesAsTibble <- function(x, var = "range", drop.existing = FALSE) {
-    stopifnot(is(x, "GRanges"))
-    df <- as.data.frame(mcols(x))
-    cols <- colnames(df)
-    if (!drop.existing & var %in% cols)
-        stop("The column ", var, " already exists")
-    df[[var]] <- as.character(granges(x))
-    as_tibble(df[unique(c(var, cols))])
-}
-
 #' @importFrom tibble as_tibble
-#' @importFrom GenomicRanges granges
-#' @importFrom S4Vectors mcols
-#' @importFrom methods slotNames
+#' @importFrom methods as
 #'
+#' @rdname as_tibble-methods
 #' @export
-#' @rdname rangesAsTibble
-mcolsAsTibble <- function(x) {
-    stopifnot(is(x, "Vector"))
-    stopifnot("elementMetadata" %in% slotNames(x))
-    df <- as.data.frame(mcols(x))
-    as_tibble(df)
+as_tibble.DataFrame <- function(x, ...) {
+    as_tibble(as.data.frame(x))
+}
+#' @importFrom rlang ':='
+#' @importFrom tibble tibble as_tibble
+#' @importFrom dplyr bind_cols
+#' @rdname as_tibble-methods
+#' @export
+as_tibble.GenomicRanges <- function(x, name = "range", rangeAsChar = TRUE, ...) {
+    if (!rangeAsChar) return(as_tibble(as.data.frame(x)))
+    gr_tbl <- tibble("{name}" := as.character(x))
+    if (ncol(mcols(x)) == 0) return(gr_tbl)
+    mc_tbl <- as_tibble(mcols(x))
+    bind_cols(gr_tbl, mc_tbl)
+
 }
--- tests/testthat/test_tibbleConversion.R
+++ tests/testthat/test_tibbleConversion.R
@@ -1,17 +1,9 @@
 test_that("drop.existing behaves correctly", {
     gr <- GRanges("chr1:1-10")
     gr$p <- 0.2
-    df <- rangesAsTibble(gr)
-    expect_error(rangesAsTibble(gr, "p"))
-    expect_error(rangesAsTibble(df))
+    df <- as_tibble(gr)
     expect_true(is(df, "tbl_df"))
-    expect_true(all(c("range", "p") %in% colnames(df)))
+    expect_equal(c("range", "p"), colnames(df))
+    expect_equal(ncol(as_tibble(gr, rangeAsChar = FALSE)), 6)
 })
 
-test_that("mcolsAsTibble behaves correctly", {
-    gr <- GRanges("chr1:1-10")
-    gr$p <- 0.2
-    df <- mcolsAsTibble(gr)
-    expect_true(colnames(df) == "p")
-    expect_error(mcolsAsTibble(df))
-})
