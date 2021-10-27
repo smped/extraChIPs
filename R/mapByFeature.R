3bc3f48d7da068573e192209ac92d81deccc5d9e
Steve Pederson <stephen.pederson.au@gmail.com>
2021-10-27 14:59:19 +1030

    Updated tests & added better checks for mapping

--- R/mapByFeature.R
+++ R/mapByFeature.R
@@ -24,21 +24,22 @@
 #' @return
 #' A GRanges object with added mcols as specified
 #'
-#' @param gr GRanges object with ranges to be mapped to genes
+#' @param gr GRanges object with query ranges to be mapped to genes
+#' @param genes GRanges object containing genes (or any other nominal feature)
+#' to be assigned
 #' @param prom GRanges object defining promoters
 #' @param enh GRanges object defining Enhancers
 #' @param gi GInteractions object defining interactions. Mappings from
 #' interactions to genes should be performed as a separate prior step.
-#' @param genes GRanges object containing genes (or any other nominal feature)
-#' to be assigned
 #' @param cols Column names to be assigned as mcols in the output. Columns
-#' must be present in both `gi` and `genes` if using both objects as
-#' references. Any columns not found in reference objects will be ignored
-#' @param range2prom The maximum permissible distance between a query range and
+#' must be minimally present in `genes`. If all requested colunms are found in
+#' any of prom, enh or gi, these pre-existing mappings will be preferentially
+#' used. Any columns not found in utilised reference objects will be ignored.
+#' @param gr2prom The maximum permissible distance between a query range and
 #' any ranges defined as promoters
-#' @param range2enh The maximum permissible distance between a query range and
+#' @param gr2enh The maximum permissible distance between a query range and
 #' any ranges defined as enhancers
-#' @param range2gi The maximum permissible distance between a query range and
+#' @param gr2gi The maximum permissible distance between a query range and
 #' any ranges defined as GInteraction anchors
 #' @param prom2gene The maximum permissible distance between a range provided
 #' in `prom` and a gene
@@ -46,9 +47,20 @@
 #' in `enh` and a gene
 #' @param gi2gene The maximum permissible distance between a GInteractions
 #' anchor (provided in `gi`) and a gene
-#' @param range2gene The maximum permissible distance between a query range and
+#' @param gr2gene The maximum permissible distance between a query range and
 #' genes (for ranges not otherwise mapped)
 #' @param ... Passed to findOverlaps nad overlapsAny internally
+#'
+#' @examples
+#' ## Define some genes
+#' genes <- GRanges(c("chr1:2-10:*", "chr1:25-30:-", "chr1:31-40:+"))
+#' genes$gene_id <- paste0("gene", seq_along(genes))
+#' ## Add a promoter for each gene
+#' prom <- promoters(genes, upstream = 1, downstream = 1)
+#' ## Some ranges to map
+#' gr <- GRanges(paste0("chr1:", seq(0, 50, by = 10)))
+#' mapByFeature(gr, genes)
+#' mapByFeature(gr, genes, prom)
 #'
 #' @importFrom S4Vectors mcols subjectHits
 #' @importFrom dplyr bind_rows distinct across
@@ -57,37 +69,50 @@
 #' @importFrom GenomicRanges findOverlaps GRanges
 #' @importFrom methods as
 #' @importFrom stats setNames
+#'
+#' @export
 mapByFeature <- function(
   gr, genes, prom, enh, gi, cols = c("gene_id", "gene_name", "symbol"),
-  range2prom = 0, range2enh = 0, range2gi = 0, range2gene = 1e5,
+  gr2prom = 0, gr2enh = 0, gr2gi = 0, gr2gene = 1e5,
   prom2gene = 0, enh2gene = 1e5, gi2gene = 0, ...
 ) {
+
+  ## Add some checks here
+  checkArgs <- .checkMappingArgs(
+    .gr = gr, .genes = genes, .prom = prom, .enh = enh, .gi = gi, .cols = cols,
+    .distArgs = list(
+      gr2prom, gr2enh, gr2gi, gr2gene, prom2gene, enh2gene, gi2gene
+    )
+  )
+  if (!checkArgs) stop("Incorrectly specified arguments")
 
   ## Perform mapping in a sequential manner as described in the algorithm
   ## First identify the mappings from gr to features
   ol_prom <- ol_enh <- ol_gi <- rep(FALSE, length(gr))
-  if (!missing(prom)) ol_prom <- overlapsAny(gr, prom, maxgap = range2prom, ...)
-  if (!missing(enh)) ol_enh <- overlapsAny(gr, enh, maxgap = range2enh, ...)
-  if (!missing(gi)) ol_gi <- overlapsAny(gr, gi, maxgap = range2gi, ...)
+  if (!missing(prom)) ol_prom <- overlapsAny(gr, prom, maxgap = gr2prom, ...)
+  if (!missing(enh)) ol_enh <- overlapsAny(gr, enh, maxgap = gr2enh, ...)
+  if (!missing(gi)) ol_gi <- overlapsAny(gr, gi, maxgap = gr2gi, ...)
   ol_none <- !ol_prom & !ol_enh & !ol_gi
 
   ## 1. Map promoters
   prom_tbl <- .mapFeatures(
-    gr[ol_prom,], prom, genes, cols, range2prom, prom2gene, ...
+    gr[ol_prom,], prom, genes, cols, gr2prom, prom2gene, ...
   )
   ## 2. Map enhancers
   enh_tbl <- .mapFeatures(
-    gr[ol_enh], enh, genes, cols, range2enh, enh2gene, ...
+    gr[ol_enh], enh, genes, cols, gr2enh, enh2gene, ...
   )
   ## 3. Add gi mappings
-  gi_tbl <- .mapGi(gr[ol_gi], gi, genes, cols,  range2gi, gi2gene, ...)
+  gi_tbl <- .mapGi(gr[ol_gi], gi, genes, cols,  gr2gi, gi2gene, ...)
   ## 4. Map any unmapped regions
-  misc_tbl <- .mapWithin(gr[ol_none], genes, cols, range2gene, ...)
+  misc_tbl <- .mapWithin(gr[ol_none], genes, cols, gr2gene, ...)
 
   ## Return the complete set of mappings & keep them as unique mappings
   mapped_tbl <- bind_rows(list(prom_tbl, enh_tbl, bind_rows(gi_tbl), misc_tbl))
   cols <- intersect(cols, names(mapped_tbl))
-  mapped_tbl <- distinct(mapped_tbl, across(all_of(cols)), .keep_all = TRUE)
+  mapped_tbl <- distinct(
+    mapped_tbl, across(all_of(c("range", cols))), .keep_all = TRUE
+  )
   mapped_tbl <- chop(mapped_tbl, all_of(cols))
   ## Prepare for returning as an addition to the original GRanges
   index <- findOverlaps(gr, GRanges(mapped_tbl[["range"]]), ...)
@@ -100,9 +125,11 @@ mapByFeature <- function(
   )
   grl <- as.list(split(gr, f = seq_along(gr) %in% queryHits(index)))
   mcols(grl[["TRUE"]])[cols] <- mapped_list
-  empty_list <- vector("list", length(grl[["FALSE"]]))
-  unmapped_list <- lapply(mapped_list, function(x) as(empty_list, is(x)[[1]]))
-  mcols(grl[["FALSE"]])[cols] <- unmapped_list
+  if (!is.null(grl[["FALSE"]])) {
+    empty_list <- vector("list", length(grl[["FALSE"]]))
+    unmapped_list <- lapply(mapped_list, function(x) as(empty_list, is(x)[[1]]))
+    mcols(grl[["FALSE"]])[cols] <- unmapped_list
+  }
   unsorted <- setNames(unlist(GRangesList(grl)), NULL)
   o <- subjectHits(findOverlaps(gr, unsorted))
   stopifnot(length(o) == length(gr))
@@ -117,53 +144,65 @@ mapByFeature <- function(
 #' @param .cols The columns from `.genes` to map onto `.gr`
 #' @param .gr2feat The maximum distance between ranges and features
 #' @param .feat2gene The maximum distance between features & genes
-#' @param ... Passed to findOverlaps
+#' @param ... Passed to findOverlaps and subsetByOverlaps
 #' @return A data.frame
 #' @importFrom S4Vectors mcols
 #' @importFrom GenomicRanges findOverlaps
-#' @importFrom dplyr inner_join left_join mutate_all
+#' @importFrom IRanges subsetByOverlaps
+#' @importFrom dplyr inner_join mutate_all
 #' @importFrom vctrs vec_proxy
 #' @importFrom tidyr unnest
 #' @importFrom tidyselect everything
 .mapFeatures <- function(.gr, .feat, .genes, .cols, .gr2feat, .feat2gene, ...) {
 
+  ## Restrict the data to just the overlapping subsets
   if (missing(.feat)) return(NULL)
-  if (length(.feat) == 0 | length(.gr) == 0) return(NULL)
+  if (is.null(.feat)) return(NULL)
+  .gr <- subsetByOverlaps(.gr, .feat, maxgap = .gr2feat)
+  if (length(.gr) == 0) return(NULL)
+  .feat <- subsetByOverlaps(.feat, .gr, maxgap = .gr2feat, ...)
+  if (length(.feat) == 0) return(NULL)
+
+  ## Given that we may pull the data directly from features, check both
   if (!missing(.genes)) {
     .cols <- intersect(.cols, names(mcols(.genes)))
   } else {
     .cols <- intersect(.cols, names(mcols(.feat)))
   }
-  stopifnot(length(.cols) > 0)
+  if (length(.cols) == 0) stop("Could not find requested column")
+
   if (!all(.cols %in% names(mcols(.feat)))) {
+    ## Use the genes object here. Otherwise it is ignored
     ## Map to genes if any requested column is absent from any existing mapping
-    feat_to_gene <- findOverlaps(.feat, .genes, maxgap = .feat2gene, ...)
-    feat_to_gene <- as.data.frame(feat_to_gene)
-    feat_df <- data.frame(queryHits = seq_along(.feat))
-    feat_df <- left_join(feat_df, feat_to_gene, by = "queryHits")
+    .genes <- subsetByOverlaps(.genes, .feat, maxgap = .feat2gene, ...)
+    if (length(.genes) == 0) return(NULL) # No point proceeding from here
+    feat_df <- findOverlaps(.feat, .genes, maxgap = .feat2gene, ...)
+    feat_df <- as.data.frame(feat_df)
     .genes$subjectHits <- seq_along(.genes)
     gene_df <- as.data.frame(mcols(.genes)[c("subjectHits", .cols)])
-    feat_df <- left_join(feat_df, gene_df, by = "subjectHits")
+    feat_df <- inner_join(feat_df, gene_df, by = "subjectHits")
     feat_df <- feat_df[c("queryHits", .cols)]
+    ## Rename for mapping onto the ranges
     names(feat_df) <- gsub("queryHits", "subjectHits", names(feat_df))
   } else {
     ## Setup as the same format otherwise
     feat_df <- mcols(.feat)[.cols]
     feat_df[["subjectHits"]] <- seq_along(.feat)
     feat_df <- as.data.frame(feat_df)
-    feat_df <- mutate_all(feat_df, vec_proxy) # Remove an AsIs attributes!!!
-    feat_df <- unnest(feat_df, everything())
   }
 
-  ## Map ranges to featoters
+  ## Deal with any list columns
+  feat_df <- mutate_all(feat_df, vec_proxy) # Remove any AsIs attributes!!!
+  feat_df <- unnest(feat_df, everything())
+
+  ## Map ranges to features
   range_to_feat <- findOverlaps(.gr, .feat, maxgap = .gr2feat, ...)
   range_to_feat <- as.data.frame(range_to_feat)
-  range_df <- as_tibble(.gr)
-  range_df$queryHits <- seq_along(.gr)
+  range_df <- data.frame(range = as.character(.gr), queryHits = seq_along(.gr))
   ## This will drop any additional columns. They can be replaced back in the
   ## parent function
   range_df <- inner_join(range_df, range_to_feat, by = "queryHits")
-  range_df <- left_join(range_df, feat_df, by = "subjectHits")
+  range_df <- inner_join(range_df, feat_df, by = "subjectHits")
   range_df <- range_df[c("range", .cols)]
 
   ## Return the tibble
@@ -179,37 +218,52 @@ mapByFeature <- function(
 #' @param .gr2gi The maximum distance between ranges and anchors
 #' @param .gi2gene The maximum distance between anchors & genes
 #' @param ... Passed to findOverlaps
-#' @return List (length 2) of tibbles
+#' @return data.frame of mapped ranges
 #' @importFrom S4Vectors mcols
 #' @importFrom vctrs vec_proxy
 #' @importFrom GenomicRanges findOverlaps
+#' @importFrom IRanges subsetByOverlaps
 #' @importFrom InteractionSet anchors
-#' @importFrom dplyr inner_join left_join mutate_all
+#' @importFrom dplyr inner_join mutate_all
 #' @importFrom tidyr unnest
 #' @importFrom tidyselect everything
 .mapGi <- function(.gr, .gi, .genes, .cols, .gr2gi, .gi2gene , ...) {
 
-  if (missing(.gi) | length(.gr) == 0) return(NULL)
+  ## Restrict the data to just the overlapping subsets
+  if (missing(.gi)) return(NULL)
+  if (is.null(.gi)) return(NULL)
+  .gr <- subsetByOverlaps(.gr, .gi, maxgap = .gr2gi)
+  if (length(.gr) == 0) return(NULL)
+  .gi <- subsetByOverlaps(.gi, .gr, maxgap = .gr2gi, ...)
+  if (length(.gi) == 0) return(NULL)
+
   if (!missing(.genes)) {
     .cols <- intersect(.cols, names(mcols(.genes)))
   } else {
     .cols <- intersect(.cols, names(mcols(.gi)))
   }
-  stopifnot(length(.cols) > 0)
+  if (length(.cols) == 0) stop("Could not find requested column")
 
   if (!all(.cols %in% names(mcols(.gi)))) {
-    .gi <- subsetByOverlaps(.gi, .gr)
+    ## If any requested column is not in the interactions object, use the genes
+    ## Just add these as columns then run as per normal
+    .genes <- subsetByOverlaps(.genes, .gi, maxgap = .gi2gene)
+    if (length(.genes) == 0) return(NULL) # Exit if no genes can be mapped
     anchors_to_gene <- lapply(
-      anchors(.gi), .mapWithin, .genes = .genes, .cols = .cols, .within = 0
+      anchors(.gi),
+      .mapWithin,
+      .genes = .genes, .cols = .cols, .within = .gi2gene
     ) # This returns the anchor range, not .gr
     anchors_to_gene <- bind_rows(anchors_to_gene)
     anchors_to_gene <- mutate_all(anchors_to_gene, vec_proxy)
     anchors_to_gene <- unnest(anchors_to_gene, everything())
-    anchors_to_gene$which <- subjectHits(findOverlaps(GRanges(anchors_to_gene$range), .gi))
-    tbl <- left_join(tibble(which = seq_along(.gi)), anchors_to_gene)[c("which", .cols)]
-    tbl <- chop(tbl, all_of(.cols))
-    stopifnot(length(.gi) == nrow(tbl))
-    mapped_list <- lapply(tbl[.cols], function(x) as(as.list(x), "List"))
+    map <- findOverlaps(GRanges(anchors_to_gene$range), .gi)
+    anchors_to_gene$which <- subjectHits(map)
+    df <- data.frame(which = seq_along(.gi))
+    df <- inner_join(df, anchors_to_gene, by = "which")[c("which", .cols)]
+    df <- chop(df, all_of(.cols))
+    stopifnot(length(.gi) == nrow(df))
+    mapped_list <- lapply(df[.cols], function(x) as(as.list(x), "List"))
     mcols(.gi)[.cols] <- mapped_list
   }
 
@@ -220,12 +274,11 @@ mapByFeature <- function(
   mapped_df <- mutate_all(mapped_df, vec_proxy) # Remove AsIs attributes!!!
   mapped_df <- unnest(mapped_df, everything())
   range_to_gi <- as.data.frame(findOverlaps(.gr, .gi, maxgap = .gr2gi, ...))
-  range_df <- as_tibble(.gr)
-  range_df$queryHits <- seq_along(.gr)
+  range_df <- data.frame(range = as.character(.gr), queryHits = seq_along(.gr))
   ## This will drop any additional columns. They can be replaced back in the
   ## parent function
   range_df <- inner_join(range_df, range_to_gi, by = "queryHits")
-  range_df <- left_join(range_df, mapped_df, by = "subjectHits")
+  range_df <- inner_join(range_df, mapped_df, by = "subjectHits")
   range_df <- range_df[c("range", .cols)]
   return(range_df)
   ## Looks right. Needs more testing
@@ -242,7 +295,7 @@ mapByFeature <- function(
 #' @importFrom S4Vectors mcols
 #' @importFrom vctrs vec_proxy
 #' @importFrom GenomicRanges findOverlaps
-#' @importFrom dplyr inner_join left_join mutate_all
+#' @importFrom dplyr inner_join mutate_all
 #' @importFrom tidyr unnest
 #' @importFrom tidyselect everything
 .mapWithin <- function(.gr, .genes, .cols, .within, ...) {
@@ -256,11 +309,64 @@ mapByFeature <- function(
   mapped_df <- unnest(mapped_df, everything())
   range_to_genes <- findOverlaps(.gr, .genes, maxgap = .within, ...)
   range_to_genes <- as.data.frame(range_to_genes)
-  range_df <- as_tibble(.gr)
-  range_df$queryHits <- seq_along(.gr)
+  range_df <- data.frame(range = as.character(.gr), queryHits = seq_along(.gr))
   ## This will drop any additional columns. They can be replaced in the
   ## parent function
   range_df <- inner_join(range_df, range_to_genes, by = "queryHits")
-  range_df <- left_join(range_df, mapped_df, by = "subjectHits")
+  range_df <- inner_join(range_df, mapped_df, by = "subjectHits")
   range_df[c("range", .cols)]
+}
+
+.checkMappingArgs <- function(.gr, .genes, .prom, .enh, .gi, .cols, .distArgs) {
+
+  if (missing(.gr)) stop("query ranges must be provided")
+  if (!is(.gr, "GRanges")) stop("query object must be a GenomicRanges object")
+  if (length(.cols) == 0) stop("At least one gene/ID column must be specified")
+  msg <- NULL
+  colInProm <- colInEnh <- colInGI <- colInGenes <- FALSE
+
+  if (!missing(.prom)) {
+    if (!is(.prom, "GRanges")) {
+      msg <- c(msg, "Promoters must be GRanges\n")
+    } else {
+      colInProm <- any(.cols %in% colnames(mcols(.prom)))
+    }
+  }
+
+  if (!missing(.enh)) {
+    if (!is(.enh, "GRanges")) {
+      msg <- c(msg, "Enhancers must be GRanges\n")
+    } else {
+      colInEnh <- any(.cols %in% colnames(mcols(.enh)))
+    }
+  }
+
+  if (!missing(.gi)) {
+    if (!is(.gi, "GInteractions")) {
+      msg <- c(msg, "Interactions must be a GInteractions object\n")
+    } else {
+      colInGI <- any(.cols %in% colnames(mcols(.gi)))
+    }
+  }
+
+  if (!missing(.genes)) {
+    ## Genes can be absent if the columns are provided in the other object
+    if (!is(.genes, "GRanges")) {
+      msg <- c(msg, "Genes must be provided as GRanges\n")
+    } else {
+      colInGenes <- any(.cols %in% colnames(mcols(.genes)))
+    }
+  }
+
+  if (!any(colInProm, colInEnh, colInGI, colInGenes))
+    msg <- c(msg, "No requested columns were able to be found\n")
+
+  .distArgs <- suppressWarnings(as.numeric(unlist(.distArgs)))
+  if (any(is.na(.distArgs)))
+    msg <- c(msg, "All distance arguments must be numeric\n")
+
+  if (is.null(msg)) return(TRUE)
+  message(msg)
+  FALSE
+
 }
--- tests/testthat/test_mapping.R
+++ tests/testthat/test_mapping.R
@@ -1,5 +1,5 @@
 # We need a combination of ranges which do & don't map
-genes <- GRanges(c("chr1:1-10:*", "chr1:25-30:-", "chr1:31-40:+"))
+genes <- GRanges(c("chr1:2-10:*", "chr1:25-30:-", "chr1:31-40:+"))
 genes$gene_id <- paste0("gene", seq_along(genes))
 ## Add a promoter for each gene
 prom <- promoters(genes, upstream = 1, downstream = 1)
@@ -11,12 +11,12 @@ gi <- InteractionSet::GInteractions(prom[1], enh[2])
 mcols(gi) <- c()
 ## Now define key ranges
 gr <- GRanges(paste0("chr1:", seq(0, 50, by = 10)))
-gr$via_prom <- CharacterList(
-  list("gene1", NULL, NULL, c("gene2", "gene3"), NULL, "gene1")
-  )
-gr$via_enh_gap0 <- CharacterList(
-  list(NULL, NULL, NULL, NULL, NULL, NULL)
+gr$via_ol <- CharacterList(
+  list(NULL, "gene1", NULL, c("gene2", "gene3"), "gene3", NULL)
 )
+gr$via_prom <- CharacterList(
+  list("gene1", NULL, NULL, c("gene2", "gene3"), NULL, NULL)
+  )
 gr$via_enh_gap5 <- CharacterList(
   list(NULL, NULL, "gene2", NULL, NULL, NULL)
 )
@@ -26,37 +26,127 @@ gr$via_gi <- CharacterList(
 
 test_that(".mapFeatures produces expected output from promoters", {
   mapped_prom <- .mapFeatures(gr, prom, genes, "gene_id", 0, 0)
-  expect_true(is(mapped_prom, "tbl_df"))
+  expect_true(is(mapped_prom, "data.frame"))
   expect_equal(
     table(mapped_prom$range),
-    structure(1:2, .Dim = 2L, .Dimnames = structure(list(c("chr1:0", "chr1:30")), .Names = ""), class = "table")
+    structure(
+      1:2, .Dim = 2L,
+      .Dimnames = structure(list(c("chr1:0", "chr1:30")), .Names = ""),
+      class = "table"
+    )
   )
   expect_equal(mapped_prom$gene_id, c("gene1", "gene2", "gene3"))
 })
 
 test_that(".mapFeatures produces expected output from enhancers", {
-  expect_equal(
-    .mapFeatures(gr, enh, genes, "gene_id", 0, 0)$gene_id, rep(NA_character_, 2)
-  )
-  expect_equal(
-    .mapFeatures(gr, enh, genes, "gene_id", 0, 5)$gene_id,
-    c("gene2", NA)
-  )
+  expect_equal(.mapFeatures(gr, enh, genes, "gene_id", 0, 5)$gene_id, "gene2")
 })
 
 test_that(".mapFeatures returns NULL & errors where expected", {
+  expect_null(.mapFeatures(gr, enh, genes, "gene_id", 0, 0))
   expect_null(.mapFeatures(NULL))
   expect_null(.mapFeatures(gr, NULL))
   expect_null(.mapFeatures(gr))
   expect_error(.mapFeatures(gr, prom, genes, "", 0, 0))
 })
 
-# test_that(".mapGi produces the expected output",{
-#
-# })
+test_that(".mapGi produces the expected output",{
+  mapped_gi <- .mapGi(gr, gi, genes, "gene_id", 0, 0)
+  expect_true(is(mapped_gi, "data.frame"))
+  expect_equal(
+    table(mapped_gi$range),
+    structure(
+      c(`chr1:0` = 1L, `chr1:50` = 1L), .Dim = 2L,
+      .Dimnames = structure(list(c("chr1:0", "chr1:50")), .Names = ""),
+      class = "table"
+    )
+  )
+  expect_equal(mapped_gi$gene_id, c("gene1", "gene1"))
+})
+
+test_that("mapGi returns NULL & errors where expected", {
+  expect_error(.mapGi(gr, gi, genes, "", 0, 0))
+  expect_null(.mapGi(gr[NULL], gi, genes, "gene_id", 0, 0))
+  expect_null(.mapGi(gr, gi[NULL], genes, "gene_id", 0, 0))
+})
+
+test_that(".mapWithin produces the expected output", {
+  mapped_within <- .mapWithin(gr, genes, "gene_id", 0)
+  expect_true(is(mapped_within, "data.frame"))
+  expect_equal(
+    table(mapped_within$range),
+    structure(
+      c(`chr1:10` = 1L, `chr1:30` = 2L, `chr1:40` = 1L), .Dim = 3L,
+      .Dimnames = structure(list(c("chr1:10", "chr1:30", "chr1:40")), .Names = ""),
+      class = "table"
+    )
+  )
+  expect_equal(mapped_within$gene_id, paste0("gene", c(1, 2, 3, 3)))
+})
 
 test_that(".mapWithin returns NULL & errors when expected", {
   expect_null(.mapWithin(NULL))
   expect_null(.mapWithin(gr))
   expect_error(.mapWithin(gr, genes, ""))
+})
+
+test_that("mapByFeature produces the correct output", {
+  ol_only <- mapByFeature(gr, genes, cols = "gene_id", gr2gene = 0)$gene_id
+  expected <- new(
+    "SimpleCharacterList", elementType = "character", elementMetadata = NULL,
+    metadata = list(),
+    listData = list(
+      character(0), "gene1", character(0), c("gene2", "gene3"), "gene3",
+      character(0))
+  )
+  expect_equal(ol_only, expected)
+  ## Update promoter only mappings
+  expected[[1]] <- "gene1"
+  expect_equal(
+    mapByFeature(gr, genes, prom = prom, cols = "gene_id", gr2gene = 0)$gene_id,
+    expected
+  )
+  ## Update enhancer only mappings
+  expected[[3]] <- "gene2"
+  expect_equal(
+    mapByFeature(
+      gr, genes, prom = prom, enh = enh, cols = "gene_id",
+      gr2gene = 0, enh2gene = 5
+    )$gene_id,
+    expected
+  )
+  ## Update interaction mappings
+  expected[[6]] <- "gene1"
+  expect_equal(
+    mapByFeature(
+      gr, genes, prom = prom, enh = enh, gi = gi,
+      cols = "gene_id", gr2gene = 0, enh2gene = 5
+    )$gene_id,
+    expected
+  )
+
+})
+
+test_that("mapByFeature errors correctly", {
+  expect_error(mapByFeature(NULL))
+  expect_error(suppressMessages(mapByFeature(gr, genes, cols = "")))
+})
+
+test_that("mappingArg Checks behave correctly", {
+  expect_error(.checkMappingArgs(), "query ranges must be provided")
+  expect_error(
+    .checkMappingArgs(NULL), "query object must be a GenomicRanges object"
+  )
+  expect_error(
+    .checkMappingArgs(GRanges(), .cols = NULL),
+    "At least one gene/ID column must be specified"
+  )
+  expect_message(
+    .checkMappingArgs(gr, genes, prom, enh, gi, "", 0),
+    "No requested columns were able to be found"
+  )
+  expect_message(
+    .checkMappingArgs(gr, genes, prom, enh, gi, "gene_id", ""),
+    "All distance arguments must be numeric"
+  )
 })
