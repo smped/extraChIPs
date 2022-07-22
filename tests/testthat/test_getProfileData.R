799bdd7875ad6115e50f29bf0edad2fee5c15579
Steve Pederson <stephen.pederson.au@gmail.com>
2022-07-22 14:41:50 +0930

    Added log-transform to `getProfileData`

--- tests/testthat/test_getProfileData.R
+++ tests/testthat/test_getProfileData.R
@@ -1,9 +1,9 @@
 bw <- system.file("tests", "test.bw", package = "rtracklayer")
-gr <- GRanges("chr2:500")
+gr <- GRanges("chr2:1000")
 
 test_that("Check structure of output", {
   bwfl <- BigWigFileList(c(a = bw, b = bw))
-  out <- getProfileData(bwfl, gr, upstream = 100, bins = 10)
+  out <- getProfileData(bwfl, gr, upstream = 500, bins = 10)
   expect_equal(names(out), c("a", "b"))
   expect_s4_class(out, "GRangesList")
   expect_s4_class(out$a$profile_data, "SplitDataFrameList")
@@ -13,7 +13,15 @@ test_that("Check structure of output", {
   expect_equal(dim(out$a$profile_data[[1]]), c(10, 3))
 })
 
+test_that("log transformation works", {
+    pd <- getProfileData(bw, gr, upstream = 500, bins = 10, log = FALSE)
+    expect_equal(range(pd$profile_data[[1]]$score), c(-0.75, 0))
+    pd <- getProfileData(bw, gr, upstream = 500, bins = 10, log = TRUE)
+    expect_equal(range(pd$profile_data[[1]]$score), c(-2, 0))
+})
+
 test_that("Errors", {
   expect_error(getProfileData("", gr))
   expect_error(getProfileData(bw))
+  expect_error(getProfileData(bw, gr, offset = "a"))
 })
