237dda74493aee59c71f7407d7263833f3535f90
Steve Pederson <stephen.pederson.au@gmail.com>
2022-07-22 16:42:58 +0930

    Changed `plotPie()` to call `ggforce::stat_pie()` directly

--- R/plotPie.R
+++ R/plotPie.R
@@ -100,75 +100,75 @@
 #' @rdname plotPie-methods
 #' @export
 setMethod(
-  "plotPie",
-  signature = signature(object = "GRanges"),
-  function(object, scale_by = c("n", "width"), ...){
-    df <- as.data.frame(object)
-    scale_by <- match.arg(scale_by)
-    if (scale_by == "n") df[["n"]] <- 1
-    ## Set width to be in Kb by default
-    if (scale_by == "width") df[["width"]] <- width(object) / 1e3
-    plotPie(df, scale_by = scale_by, ...)
-  }
+    "plotPie",
+    signature = signature(object = "GRanges"),
+    function(object, scale_by = c("n", "width"), ...){
+        df <- as.data.frame(object)
+        scale_by <- match.arg(scale_by)
+        if (scale_by == "n") df[["n"]] <- 1
+        ## Set width to be in Kb by default
+        if (scale_by == "width") df[["width"]] <- width(object) / 1e3
+        plotPie(df, scale_by = scale_by, ...)
+    }
 )
 #' @importClassesFrom S4Vectors DataFrame
 #' @rdname plotPie-methods
 #' @export
 setMethod(
-  "plotPie",
-  signature = signature(object = "DataFrame"),
-  function(object, ...){
-    object <- as.data.frame(object)
-    plotPie(object, ...)
-  }
+    "plotPie",
+    signature = signature(object = "DataFrame"),
+    function(object, ...){
+        object <- as.data.frame(object)
+        plotPie(object, ...)
+    }
 )
 #'
 #' @rdname plotPie-methods
 #' @export
 setMethod(
-  "plotPie",
-  signature = signature(object = "data.frame"),
-  function(
-    object, fill, x, y, scale_by, width = 0.8, show_total = TRUE,
-    label_fill = "white", label_alpha = 1, label_size = 3, min_p = 0.01,
-    show_category = TRUE, category_size = 3, category_colour = "black",
-    category_width = 15, ...
-  ) {
+    "plotPie",
+    signature = signature(object = "data.frame"),
+    function(
+        object, fill, x, y, scale_by, width = 0.8, show_total = TRUE,
+        label_fill = "white", label_alpha = 1, label_size = 3, min_p = 0.01,
+        show_category = TRUE, category_size = 3, category_colour = "black",
+        category_width = 15, ...
+    ) {
 
-    if (missing(fill)) stop("The initial category must be defined as 'fill'\n")
+        if (missing(fill)) stop("The initial category must be defined as 'fill'\n")
 
-    if (missing(x) & missing(y)) {
-      p <- .plotSinglePie(
-        df = object, fill = fill, width = width, show_total = show_total,
-        .lab_fill = label_fill, .lab_alpha = label_alpha,
-        .lab_size = label_size, .text_size = category_size,
-        .text_col = category_colour, .min_p = min_p,
-        .show_cat = show_category, .text_width = category_width,
-        .scale_by = scale_by
-      )
+        if (missing(x) & missing(y)) {
+            p <- .plotSinglePie(
+                df = object, fill = fill, width = width, show_total = show_total,
+                .lab_fill = label_fill, .lab_alpha = label_alpha,
+                .lab_size = label_size, .text_size = category_size,
+                .text_col = category_colour, .min_p = min_p,
+                .show_cat = show_category, .text_width = category_width,
+                .scale_by = scale_by
+            )
+        }
+
+        if (missing(x) & !missing(y))
+            stop("Charts can only be drawn in rows when using two columns.\n")
+
+        if (!missing(x) & missing(y))
+            p <- .plotDoublePie(
+                df = object, x = x, fill = fill, width = width, show_total = show_total,
+                .lab_fill = label_fill, .lab_alpha = label_alpha,
+                .lab_size = label_size, .min_p = min_p, .scale_by = scale_by
+            )
+
+        if (!missing(x) & !missing(y))
+            p <- .plotTriplePie(
+                df = object, x = x, y = y, fill = fill, width = width,
+                show_total = show_total, .lab_fill = label_fill,
+                .lab_alpha = label_alpha, .lab_size = label_size, .min_p = min_p,
+                .scale_by = scale_by
+            )
+
+        p
+
     }
-
-    if (missing(x) & !missing(y))
-      stop("Charts can only be drawn in rows when using two columns.\n")
-
-    if (!missing(x) & missing(y))
-      p <- .plotDoublePie(
-        df = object, x = x, fill = fill, width = width, show_total = show_total,
-        .lab_fill = label_fill, .lab_alpha = label_alpha,
-        .lab_size = label_size, .min_p = min_p, .scale_by = scale_by
-      )
-
-    if (!missing(x) & !missing(y))
-      p <- .plotTriplePie(
-        df = object, x = x, y = y, fill = fill, width = width,
-        show_total = show_total, .lab_fill = label_fill,
-        .lab_alpha = label_alpha, .lab_size = label_size, .min_p = min_p,
-        .scale_by = scale_by
-      )
-
-    p
-
-  }
 )
 
 #' @importFrom dplyr group_by summarise mutate arrange filter
@@ -179,56 +179,56 @@ setMethod(
 #' @importFrom stringr str_wrap
 #' @importFrom tidyr complete
 .plotSinglePie <- function(
-  df, fill, width , show_total, .lab_fill, .lab_alpha, .lab_size, .text_size,
-  .text_col, .min_p, .show_cat, .text_width, .scale_by
+        df, fill, width , show_total, .lab_fill, .lab_alpha, .lab_size, .text_size,
+        .text_col, .min_p, .show_cat, .text_width, .scale_by
 ) {
 
-  fill <- fill[[1]]
-  stopifnot(fill %in% colnames(df))
-  df[[fill]] <- as.factor(df[[fill]])
-  if (!missing(.scale_by)) stopifnot(is.numeric(df[[.scale_by]]))
-  y <- lab <- c() # R CMD check error avoidance
+    fill <- fill[[1]]
+    stopifnot(fill %in% colnames(df))
+    df[[fill]] <- as.factor(df[[fill]])
+    if (!missing(.scale_by)) stopifnot(is.numeric(df[[.scale_by]]))
+    y <- lab <- c() # R CMD check error avoidance
 
-  grp_df <- group_by(df, !!sym(fill))
-  if (missing(.scale_by)) {
-    summ_df <- summarise(grp_df, n = dplyr::n(), .groups = "drop")
-  } else {
-    summ_df <- summarise(grp_df, n = sum(!!sym(.scale_by)))
-  }
-  summ_df <- complete(summ_df, !!sym(fill), fill = list(n = 0))
-  summ_df <- mutate(summ_df, p = n / sum(n))
-  summ_df <- arrange(summ_df, desc(!!sym(fill)))
-  summ_df$y <- cumsum(summ_df$p)
-  summ_df$lab <- paste0(
-    summ_df[[fill]], " (", percent(summ_df$p, 0.1), ")"
-  )
-  summ_df$lab <- str_wrap(summ_df$lab, width = .text_width)
-
-  p <- ggplot(data = summ_df, aes(1, p, fill = !!sym(fill))) +
-    geom_col(width = width)
-
-  if (.show_cat) {
-    p <- p + geom_text(
-      aes(x = 1.2 + width / 2, y = y - 0.5*p, label = lab),
-      data = dplyr::filter(summ_df, p > .min_p),
-      colour = .text_col, size = .text_size
-    ) +
-    geom_segment(
-      aes(
-        x = 1 + width / 2, xend = 1.1 + width / 2,
-        y = y - 0.5*p, yend = y - 0.5*p
-      ),
-      data = dplyr::filter(summ_df, p > .min_p)
+    grp_df <- group_by(df, !!sym(fill))
+    if (missing(.scale_by)) {
+        summ_df <- summarise(grp_df, n = dplyr::n(), .groups = "drop")
+    } else {
+        summ_df <- summarise(grp_df, n = sum(!!sym(.scale_by)))
+    }
+    summ_df <- complete(summ_df, !!sym(fill), fill = list(n = 0))
+    summ_df <- mutate(summ_df, p = n / sum(n))
+    summ_df <- arrange(summ_df, desc(!!sym(fill)))
+    summ_df$y <- cumsum(summ_df$p)
+    summ_df$lab <- paste0(
+        summ_df[[fill]], " (", percent(summ_df$p, 0.1), ")"
     )
-  }
+    summ_df$lab <- str_wrap(summ_df$lab, width = .text_width)
 
-  if (show_total)
-    p <- p + geom_label(
-      x = 1 - width / 2, y = 0, label = comma(sum(summ_df$n), 1),
-      alpha = 0.5, fill = "white", size = .lab_size
-    )
+    p <- ggplot(data = summ_df, aes(1, p, fill = !!sym(fill))) +
+        geom_col(width = width)
 
-  p + coord_polar("y", start = 0) + theme_void()
+    if (.show_cat) {
+        p <- p + geom_text(
+            aes(x = 1.2 + width / 2, y = y - 0.5*p, label = lab),
+            data = dplyr::filter(summ_df, p > .min_p),
+            colour = .text_col, size = .text_size
+        ) +
+            geom_segment(
+                aes(
+                    x = 1 + width / 2, xend = 1.1 + width / 2,
+                    y = y - 0.5*p, yend = y - 0.5*p
+                ),
+                data = dplyr::filter(summ_df, p > .min_p)
+            )
+    }
+
+    if (show_total)
+        p <- p + geom_label(
+            x = 1 - width / 2, y = 0, label = comma(sum(summ_df$n), 1),
+            alpha = 0.5, fill = "white", size = .lab_size
+        )
+
+    p + coord_polar("y", start = 0) + theme_void()
 }
 
 #' @importFrom dplyr group_by summarise mutate filter ungroup distinct
@@ -238,64 +238,68 @@ setMethod(
 #' @importFrom ggplot2 coord_equal labs
 #' @importFrom scales comma
 #' @importFrom rlang '!!' sym
-#' @importFrom scatterpie geom_scatterpie
+#' @importFrom ggforce stat_pie
 .plotDoublePie <- function(
-  df, x, fill, width, show_total, .lab_fill, .lab_alpha, .lab_size, .min_p,
-  .scale_by
+        df, x, fill, width, show_total, .lab_fill, .lab_alpha, .lab_size, .min_p,
+        .scale_by
 ) {
 
-  fill <- fill[[1]]
-  x <- x[[1]]
-  stopifnot(all(c(x, fill) %in% colnames(df)))
-  df[[x]] <- as.factor(df[[x]])
-  df[[fill]] <- as.factor(df[[fill]])
-  if (!missing(.scale_by)) stopifnot(is.numeric(df[[.scale_by]]))
-  r <- N <- value <- p <- c() # R CMD check error avoidance
+    fill <- fill[[1]]
+    x <- x[[1]]
+    stopifnot(all(c(x, fill) %in% colnames(df)))
+    df[[x]] <- as.factor(df[[x]])
+    df[[fill]] <- as.factor(df[[fill]])
+    if (!missing(.scale_by)) stopifnot(is.numeric(df[[.scale_by]]))
+    r <- N <- value <- p <- c() # R CMD check error avoidance
 
-  grp_df <- group_by(df, !!sym(x), !!sym(fill))
-  if (missing(.scale_by)) {
-    ## The column 'value' is hard-wired into geom_scatterpie for long format
-    summ_df <- summarise(grp_df, value = dplyr::n(), .groups = "drop_last")
-  } else {
-    summ_df <- summarise(
-      grp_df, value = sum(!!sym(.scale_by)), .groups = "drop_last"
+    grp_df <- group_by(df, !!sym(x), !!sym(fill))
+    if (missing(.scale_by)) {
+        ## The column 'value' is hard-wired into geom_scatterpie for long format
+        summ_df <- summarise(grp_df, value = dplyr::n(), .groups = "drop_last")
+    } else {
+        summ_df <- summarise(
+            grp_df, value = sum(!!sym(.scale_by)), .groups = "drop_last"
+        )
+    }
+    summ_df <- mutate(
+        summ_df,
+        p = value / sum(value), label_radians = 2 * pi * (cumsum(p) - 0.5 * p)
     )
-  }
-  summ_df <- mutate(
-    summ_df,
-    p = value / sum(value), label_radians = 2 * pi * (cumsum(p) - 0.5 * p)
-  )
-  summ_df <- ungroup(mutate(summ_df, N = sum(value)))
-  summ_df <- complete(
-    summ_df, !!sym(fill), !!sym(x),
-    fill = list(value = 0, N = 0, p = 0, label_radians = 0)
-  )
-  summ_df$r <- summ_df$N / sum(summ_df$N)
-  summ_df$r <- 0.5 * summ_df$r / max(summ_df$r) # Set the max as 0.5 always
-  summ_df$x <- as.integer(summ_df[[x]])
+    summ_df <- ungroup(mutate(summ_df, N = sum(value)))
+    summ_df <- complete(
+        summ_df, !!sym(fill), !!sym(x),
+        fill = list(value = 0, N = 0, p = 0, label_radians = 0)
+    )
+    summ_df$r <- summ_df$N / sum(summ_df$N)
+    summ_df$r <- 0.5 * summ_df$r / max(summ_df$r) # Set the max as 0.5 always
+    summ_df$x <- as.integer(summ_df[[x]])
 
-  p <- ggplot(data = summ_df) +
-    geom_scatterpie(
-      aes(x, 1, r = width * r), data = summ_df, cols = fill, long_format = TRUE
-    ) +
-    coord_equal() +
-    scale_x_continuous(
-      breaks = seq_along(levels(df[[x]])), labels = levels(df[[x]])
-    ) +
-    theme(
-      axis.text.y = element_blank(), axis.title.y = element_blank(),
-      axis.ticks.y = element_blank()
-    )
-  if (show_total) {
-    lab_df <- dplyr::filter(summ_df, N > .min_p * sum(N))
-    lab_df <- distinct(lab_df, x, N, .keep_all = TRUE)
-    p <- p + geom_label(
-      aes(x, 1, label = comma(N, 1)), data = lab_df,
-      size = .lab_size, alpha = .lab_alpha, fill = .lab_fill
-    )
-  }
+    p <- ggplot(data = summ_df) +
+        ggforce::stat_pie(
+            aes(
+                x0 = x, y0 = 1, r0 = 0, r = width * r,
+                fill = !!sym(fill), amount = value
+            )
+        ) +
+        coord_equal() +
+        scale_x_continuous(
+            breaks = seq_along(levels(df[[x]])), labels = levels(df[[x]])
+        ) +
+        theme(
+            panel.grid = element_blank(),
+            axis.text.y = element_blank(), axis.title.y = element_blank(),
+            axis.ticks.y = element_blank()
+        )
+    if (show_total) {
+        lab_df <- dplyr::filter(summ_df, N > .min_p * sum(N))
+        lab_df <- distinct(lab_df, x, N, .keep_all = TRUE)
+        p <- p + geom_label(
+            aes(x, 1, label = comma(N, 1)), data = lab_df,
+            size = .lab_size, alpha = .lab_alpha, fill = .lab_fill
+        )
+    }
 
-  p + labs(x = x, fill = fill)
+    p + labs(x = x, fill = fill)
 
 }
 
@@ -306,66 +310,69 @@ setMethod(
 #' @importFrom ggplot2 coord_equal labs scale_y_continuous
 #' @importFrom scales comma
 #' @importFrom rlang '!!' sym
-#' @importFrom scatterpie geom_scatterpie
+#' @importFrom ggforce stat_pie
 .plotTriplePie <- function(
-  df, x, y, fill, width, show_total, .lab_fill, .lab_alpha, .lab_size, .min_p,
-  .scale_by
+        df, x, y, fill, width, show_total, .lab_fill, .lab_alpha, .lab_size, .min_p,
+        .scale_by
 ) {
 
-  fill <- fill[[1]]
-  x <- x[[1]]
-  y <- y[[1]]
-  stopifnot(all(c(x, y, fill) %in% colnames(df)))
+    fill <- fill[[1]]
+    x <- x[[1]]
+    y <- y[[1]]
+    stopifnot(all(c(x, y, fill) %in% colnames(df)))
 
-  df[[x]] <- as.factor(df[[x]])
-  df[[y]] <- as.factor(df[[y]])
-  df[[fill]] <- as.factor(df[[fill]])
-  if (!missing(.scale_by)) stopifnot(is.numeric(df[[.scale_by]]))
-  r <- N <- p <- value <- c() # R CMD check error avoidance
+    df[[x]] <- as.factor(df[[x]])
+    df[[y]] <- as.factor(df[[y]])
+    df[[fill]] <- as.factor(df[[fill]])
+    if (!missing(.scale_by)) stopifnot(is.numeric(df[[.scale_by]]))
+    r <- N <- p <- value <- c() # R CMD check error avoidance
 
-  grp_df <- group_by(df, !!sym(x), !!sym(y), !!sym(fill))
-  if (missing(.scale_by)) {
-    summ_df <- summarise(grp_df, value = dplyr::n(), .groups = "drop_last")
-  } else {
-    summ_df <- summarise(
-      grp_df, value = sum(!!sym(.scale_by)), .groups = "drop_last"
+    grp_df <- group_by(df, !!sym(x), !!sym(y), !!sym(fill))
+    if (missing(.scale_by)) {
+        summ_df <- summarise(grp_df, value = dplyr::n(), .groups = "drop_last")
+    } else {
+        summ_df <- summarise(
+            grp_df, value = sum(!!sym(.scale_by)), .groups = "drop_last"
+        )
+    }
+    summ_df <- mutate(
+        summ_df,
+        p = value / sum(value), label_radians = 2 * pi * (cumsum(p) - 0.5 * p)
     )
-  }
-  summ_df <- mutate(
-    summ_df,
-    p = value / sum(value), label_radians = 2 * pi * (cumsum(p) - 0.5 * p)
-  )
-  summ_df <- ungroup(mutate(summ_df, N = sum(value)))
-  summ_df <- complete(
-    summ_df, !!sym(fill), !!sym(x), !!sym(y),
-    fill = list(value = 0, N = 0, p = 0, label_radians = 0)
-  )
-  summ_df$r <- summ_df$N / sum(summ_df$N)
-  summ_df$r <- 0.5 * summ_df$r / max(summ_df$r) # Set the max as 0.5 always
-  summ_df$x <- as.integer(summ_df[[x]])
-  summ_df$y <- as.integer(summ_df[[y]])
-
-  p <- ggplot(data = summ_df) +
-    geom_scatterpie(
-      aes(x, y, r = width * r), data = summ_df, cols = fill, long_format = TRUE
-    ) +
-    coord_equal() +
-    scale_x_continuous(
-      breaks = seq_along(levels(df[[x]])), labels = levels(df[[x]])
-    ) +
-    scale_y_continuous(
-      breaks = seq_along(levels(df[[y]])), labels = levels(df[[y]])
+    summ_df <- ungroup(mutate(summ_df, N = sum(value)))
+    summ_df <- complete(
+        summ_df, !!sym(fill), !!sym(x), !!sym(y),
+        fill = list(value = 0, N = 0, p = 0, label_radians = 0)
     )
-  if (show_total) {
-    lab_df <- dplyr::filter(summ_df, N > .min_p * sum(N))
-    lab_df <- distinct(lab_df, x, y, N, .keep_all = TRUE)
-    p <- p + geom_label(
-      aes(x, y, label = comma(N, 1)), data = lab_df,
-      size = .lab_size, alpha = .lab_alpha, fill = .lab_fill
-    )
-  }
+    summ_df$r <- summ_df$N / sum(summ_df$N)
+    summ_df$r <- 0.5 * summ_df$r / max(summ_df$r) # Set the max as 0.5 always
+    summ_df$x <- as.integer(summ_df[[x]])
+    summ_df$y <- as.integer(summ_df[[y]])
 
-  p + labs(x = x, y = y, fill = fill)
+    p <- ggplot(data = summ_df) +
+        ggforce::stat_pie(
+            aes(
+                x0 = x, y0 = y, r0 = 0, r = width * r,
+                fill = !!sym(fill), amount = value
+            )
+        ) +
+        coord_equal() +
+        scale_x_continuous(
+            breaks = seq_along(levels(df[[x]])), labels = levels(df[[x]])
+        ) +
+        scale_y_continuous(
+            breaks = seq_along(levels(df[[y]])), labels = levels(df[[y]])
+        )
+    if (show_total) {
+        lab_df <- dplyr::filter(summ_df, N > .min_p * sum(N))
+        lab_df <- distinct(lab_df, x, y, N, .keep_all = TRUE)
+        p <- p + geom_label(
+            aes(x, y, label = comma(N, 1)), data = lab_df,
+            size = .lab_size, alpha = .lab_alpha, fill = .lab_fill
+        )
+    }
+
+    p + labs(x = x, y = y, fill = fill)
 
 }
 
