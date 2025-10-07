333a0ada5d18029d4a22ec3b1aecbb3f825b37fd
Stevie Ped <stephen.pederson.au@gmail.com>
2025-10-07 23:59:58 +1030

    BiocCheck-based edit

--- R/fitAssayDiff.R
+++ R/fitAssayDiff.R
@@ -22,7 +22,7 @@
 #' If testing with \link[DESeq2]{nbinomWaldTest}, applying RLE normalisation
 #' without groups, and using colSums for library sizes (instead of total
 #' alignments), the standard normalisation factors from
-#' estimateSizeFactors (DESeq2) will be used.
+#' `DESeq2::estimateSizeFactors` will be used.
 #' In all other scenarios, normalisation factors as returned by
 #' \link[edgeR]{normLibSizes} will be used.
 #' The fitType is set to 'local' when estimating dispersions, and this can be
